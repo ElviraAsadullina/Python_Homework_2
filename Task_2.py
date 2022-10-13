725956021c0c7b64e038842c9fe9ad1af3ae109c
Elvira <eliasufa@yahoo.com>
2022-10-13 17:48:32 +0500

    Задача 2 решена

---
+++ Task_2.py
@@ -0,0 +1,29 @@
+def check_for_input_error():
+    while True:
+        try:
+            a = int(input('Введите любое натуральное число: '))
+        except ValueError:
+            print('Ошибка - необходимо ввести натуральное число!')
+            continue
+        if  a < 1:
+            print('Ошибка - число должно быть положительным!')
+            continue
+        break
+    return a
+def list_from_int(a):
+    listed_a = []
+    stringed_a = str(a)
+    for i in stringed_a:
+        listed_a.append(int(i))
+    return listed_a
+def get_factorial(a):
+    factorial = 1
+    for i in a:
+        factorial *= i
+    return factorial
+N = check_for_input_error()
+# listed_N = list_from_int(N)
+print(f'Факториал введенного числа = {get_factorial(list_from_int(N))}')
+
+# listed_N = list_from_int(N)
+# print(f'Факториал введенного числа = {get_factorial(listed_N)}')
